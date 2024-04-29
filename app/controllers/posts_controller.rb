3e0e58ef7d583b7bd9c9888e7ca0e9ee46162509
NicoMaldos <45697877+NicoMaldos@users.noreply.github.com>
2024-04-29 11:07:07 -0400

    feature(post): add endpoints to create, delete and get

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,23 @@
 class PostsController < ApplicationController
+    def index
+        posts = Post.all
+        render json: posts, status:200
+    end
+
+    def destroy
+        Post.find(params[:id]).destroy
+        posts = Post.all
+        render json: posts
+      end
+
+    def create
+        post = Post.create!(post_params)
+        render json: post, status: :created
+    end
+
+    private
+
+    def post_params
+        params.permit(:name, :description)
+    end
 end
